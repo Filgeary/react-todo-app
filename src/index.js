8f5436df7d3a35ba4e6b5670192cf366abbe489d
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-29 17:35:37 +0500

    create first react components

--- src/index.js
+++ src/index.js
@@ -3,19 +3,32 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
-const elem = (
-  <div>
-    <h1>My ToDo List</h1>
-    <input type="search" name="search" id="search" placeholder="Search" />
+const AppHeader = () => <h1>My ToDo List</h1>
 
+const SearchPanel = () => {
+  return <input type="search" name="search" id="search" placeholder="Search" />
+}
+
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
     </ul>
-  </div>
-)
+  )
+}
 
-ReactDOM.render(elem, document.getElementById('root'))
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
