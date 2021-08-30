4618a72bcb6184a81698591019a4a8f8942b16fc
Georgiy Filippov <g.filippov777@gmail.com>
2021-08-30 17:29:40 +0500

    add props

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,8 +4,11 @@ import TodoListItem from './TodoListItem'
 const TodoList = () => {
   return (
     <ul>
-      <li>{<TodoListItem />}</li>
-      <li>{<TodoListItem />}</li>
+      <li>{<TodoListItem label="Learn React" />}</li>
+      <li>{<TodoListItem label="Learn Redux" />}</li>
+      <li>{<TodoListItem label="Write Tests" />}</li>
+      <li>{<TodoListItem label="Learn TypeScript" />}</li>
+      <li>{<TodoListItem label="Find a Cool Job!" important />}</li>
     </ul>
   )
 }
--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
-const TodoListItem = () => {
-  return <span>Learn React</span>
+const TodoListItem = ({ label, important = false }) => {
+  const styles = {
+    color: important ? 'tomato' : '',
+  }
+
+  return <span style={styles}>{label}</span>
 }
 
 export default TodoListItem
