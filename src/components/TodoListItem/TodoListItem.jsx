b1bd7c2b9a4d960a76a90a8ca1dd05a408719935
Georgiy Filippov <g.filippov777@gmail.com>
2021-09-02 17:41:20 +0500

    add state

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -2,20 +2,27 @@ import React from 'react'
 import './TodoListItem.css'
 
 class TodoListItem extends React.Component {
+  state = {
+    done: false,
+  }
+
   handleClickLabel = () => {
-    console.log(this.props.label)
+    this.setState({ done: true })
   }
 
   render() {
     const { label, isImportant = false } = this.props
+    const { done } = this.state
 
     const styles = {
       color: isImportant ? 'steelblue' : '',
       fontWeight: isImportant ? 'bold' : 'normal',
     }
 
+    const cls = done ? 'done' : ''
+
     return (
-      <span className="todo-list-item d-flex">
+      <span className={`todo-list-item d-flex ${cls}`}>
         <span
           className="todo-list-item-label"
           style={styles}
