492dcd9cd7617300b02f7304a8b17d0cc429ddad
Georgiy Filippov <g.filippov777@gmail.com>
2021-09-04 12:19:27 +0500

    refactor - `TodoListItem` to stateless function

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,50 +1,47 @@
 import React from 'react'
 import './TodoListItem.css'
 
-class TodoListItem extends React.Component {
-  render() {
-    const {
-      label,
-      onDeleteItem,
-      onToggleDone,
-      onToggleImportant,
-      isDone,
-      isImportant,
-    } = this.props
+const TodoListItem = ({
+  label,
+  onDeleteItem,
+  onToggleDone,
+  onToggleImportant,
+  isDone,
+  isImportant,
+}) => {
+  // mix classes
+  let cls = ''
+  if (isDone) cls += ' done'
+  if (isImportant) cls += ' important'
 
-    let cls = ''
-    if (isDone) cls += ' done'
-    if (isImportant) cls += ' important'
-
-    return (
-      <span className={`todo-list-item d-flex ${cls}`}>
-        <span
-          className="todo-list-item-label"
-          tabIndex="0"
-          onClick={onToggleDone}
-        >
-          {label}
-        </span>
-
-        <div>
-          <button
-            type="button"
-            className="btn btn-outline-success btn-sm "
-            onClick={onToggleImportant}
-          >
-            <i className="fa fa-exclamation" />
-          </button>
-          <button
-            type="button"
-            className="btn btn-outline-danger btn-sm "
-            onClick={onDeleteItem}
-          >
-            <i className="fa fa-trash-o"></i>
-          </button>
-        </div>
+  return (
+    <span className={`todo-list-item d-flex ${cls}`}>
+      <span
+        className="todo-list-item-label"
+        tabIndex="0"
+        onClick={onToggleDone}
+      >
+        {label}
       </span>
-    )
-  }
+
+      <div>
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm "
+          onClick={onToggleImportant}
+        >
+          <i className="fa fa-exclamation" />
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm "
+          onClick={onDeleteItem}
+        >
+          <i className="fa fa-trash-o"></i>
+        </button>
+      </div>
+    </span>
+  )
 }
 
 export default TodoListItem
