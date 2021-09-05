1dd2bd2e82e85c3940b129285642d22f5ec6cf2e
Georgiy Filippov <g.filippov777@gmail.com>
2021-09-05 21:45:28 +0500

    add `search`

--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import './SearchPanel.css'
 
-const SearchPanel = () => {
-  return (
-    <input
-      type="text"
-      className="form-control"
-      name="search"
-      id="search"
-      placeholder="search"
-    />
-  )
+class SearchPanel extends React.Component {
+  state = {
+    searchValue: '',
+  }
+
+  handleChangeSearch = evt => {
+    const { onChangeSearch } = this.props
+
+    this.setState({ searchValue: evt.target.value })
+    onChangeSearch(evt.target.value)
+  }
+
+  render() {
+    const { searchValue } = this.state
+
+    return (
+      <input
+        type="text"
+        className="form-control"
+        name="search"
+        id="search"
+        placeholder="search"
+        value={searchValue}
+        onChange={this.handleChangeSearch}
+      />
+    )
+  }
 }
 
 export default SearchPanel
